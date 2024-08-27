d6a259a00da42a6675a05bd15363d61a46e19b0d
Lseything <lseything@proton.me>
2024-08-27 04:58:44 -0300

    insert module files

---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+/data/local/AIK-mobile/bin/aik --uninstall 2> /dev/null
+rm -rf /data/local/binary
+rm -rf /data/local/python31
+rm -rf /data/local/python39
+rm -rf /data/local/UnpackerContexts
+rm -rf /data/local/UnpackerPayload
+rm -rf /data/local/UnpackerPreloader
+rm -rf /data/local/UnpackerQfil
+rm -rf /data/local/UnpackerSystem
+rm -rf /data/local/UnpackerSuper
+rm -rf /data/local/UnpackerUpdateApp
+rm -rf /data/adb/modules/UKA
